415e40b7c0b73e896d19f990cdd112b604ed0aeb
aushakov95 <ushakoan@gmail.com>
2016-05-09 00:51:02 -0700

    Added Reset Color feature

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    var divCounter = 52;
     $('#container').append('<div></div>');
     $('#container').append('<div></div>');
     $('#container').append('<div></div>');
@@ -57,22 +58,29 @@ $(document).ready(function(){
             $(this).css('background-color', "black");
         });
     $(document).on('click', '#container > div', function() {
-                $(this).css('background-color', "blue");
+                $(this).fadeTo( "slow" , 0);
         });
     $('#newGrid').click(function(){
         $('#container').empty();
         var width = prompt("Enter the width of the grid")
         var height = prompt("Enter the height of the grid")
 
+        counter = 0;
         var counterWidth = 0;
         var counterHeight = 0;
         while (counterHeight < height){
             for(; counterWidth < width; counterWidth++){
-                $('#container').append('<div></div>');
+                $('#container').append('<div id="0"></div>');
+                divCounter++;
             }
             $('#container').append('<br>');
             counterWidth = 0;
             counterHeight++;
+        }
+    });
+    $('#colorReset').click(function(){
+        for (var i = 0; i < divCounter; i++){
+            $('#container > div#' + i).css('background-color', "blue");
         }
     });
 
